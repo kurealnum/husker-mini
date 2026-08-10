a884e36692043a97b8eae494be4029a393429d47
kurealnum <oscar.gaske.cs@gmail.com>
2026-08-09 23:14:42 -0400

    feat: add prediction_stages table
    Closes #12

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -1,5 +1,6 @@
 export * from "./_helpers";
 export * from "./predictions";
 export * from "./model-outputs";
+export * from "./prediction-stages";
 export * from "./sentiment-analyses";
 export * from "./technical-analyses";
