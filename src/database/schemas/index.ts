1171dc996aea773fa47aa3dc2282d6b9e1107cda
kurealnum <oscar.gaske.cs@gmail.com>
2026-08-09 23:17:01 -0400

    feat: add prediction_snapshots table
    Closes #14

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -2,6 +2,7 @@ export * from "./_helpers";
 export * from "./predictions";
 export * from "./model-outputs";
 export * from "./news-articles";
+export * from "./prediction-snapshots";
 export * from "./prediction-stages";
 export * from "./sentiment-analyses";
 export * from "./technical-analyses";
